426413dc44397852e766e8c7c57038067dfee1ab
Sergey-Volkonskiy <mailbox.kyiv@gmail.com>
2022-04-11 18:00:29 +0300

    added demo js code

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//lkj
+let a = 0;
+a = "Javascript is awesome".length;
+let length = 0;
+stringLength = "Javascript is awesome".length;
+"Javascript is awesome".toUpperCase();
+console.log(stringLength);
+console.log(Number.parseFloat("qwe79"))
